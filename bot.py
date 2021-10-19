b19afd5bd4d6b4afb52ac2f6fe17bb322d927d3a
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2021-10-19 16:46:34 +0300

    Improve data format and code comments

--- bot.py
+++ bot.py
@@ -21,42 +21,41 @@ class Bot(BaseBot):
         ok, r = self.api.send_message(chat_id, text, **kwargs)
         if not ok:
             if r["description"] == "Forbidden: bot was blocked by the user":
-                # kullanıcı botu silmişse sen de kullanıcıyı sil
+                # Kullanıcı botu silmişse sen de kullanıcıyı sil
                 delete_user(self, chat_id)
             else:
                 raise Exception(r)
         return ok, r
 
     def handle_update(self, update):
-        # return
         if msg := update.get("message"):
-            chat_id = msg.get("chat").get("id")
+            chat_id = msg["chat"]["id"]
             text = msg.get("text")
             print(chat_id)
             print(text)
             msg_id = msg.get("message_id")
-            if chat_id and text:
+            if text:
 
                 if text.startswith("/"):
                     if text == "/start":
                         if user_exists(self, chat_id):
-                            # kullanıcı zaten varsa
+                            # Kullanıcı zaten varsa
                             user_state = get_user(self, chat_id, "state")[0]
 
-                            if user_state == 0:  # ama bir terslik (mesela kodun *a* satırında göçmesi) sonucu state 1 olamamışsa:
-                                # tanış ama kullanıcıyı yeniden oluşturmaya çalışma!
+                            if user_state == 0:  # Ama bir terslik (mesela kodun *a* satırında göçmesi) sonucu state 1 olamamışsa:
+                                # Tanış ama kullanıcıyı yeniden oluşturmaya çalışma!
                                 self.send_msg(chat_id, mt.ask_name(), reply_markup=self.api.build_remove_keyboard())
                                 set_user(self, chat_id, "state", 1)
                             elif user_state == 6:
-                                # eğer kullanıcı /sifirla'ya cevap olarak /start girmişse vazgeçti say
+                                # Eğer kullanıcı /sifirla'ya cevap olarak /start girmişse vazgeçti say
                                 self.send_msg(chat_id, mt.user_canceled_sifirla(), reply_markup=self.api.build_remove_keyboard())
                                 set_user(self, chat_id, "state", 4)
                             elif user_state < 4:
-                                # tanışma zaten devam ediyor
+                                # Tanışma zaten devam ediyor
                                 custom_kb = build_custom_kb_for_curr_state(self, chat_id, user_state=user_state)
                                 self.send_msg(chat_id, mt.meeting_already_ongoing(), reply_markup=custom_kb)
                             else:
-                                # zaten tanıştık
+                                # Zaten tanışmıştık
                                 self.send_msg(chat_id, mt.already_met())
                         else:
                             create_user(self, chat_id)
@@ -67,11 +66,11 @@ class Bot(BaseBot):
                             user_state = get_user(self, chat_id, "state")[0]
 
                             if user_state == 6:
-                                # eğer kullanıcı /sifirla'ya cevap olarak bir komut girmişse vazgeçti say
+                                # Eğer kullanıcı /sifirla'ya cevap olarak bir komut girmişse vazgeçti say
                                 self.send_msg(chat_id, mt.user_canceled_sifirla(), reply_markup=self.api.build_remove_keyboard())
                                 set_user(self, chat_id, "state", 4)
                             elif user_state != 4:
-                                # diğer komutları işleme!, önce mevcut olanı tamamla
+                                # Diğer komutları işleme!, önce mevcut olanı tamamla
                                 custom_kb = build_custom_kb_for_curr_state(self, chat_id, user_state=user_state)
                                 self.send_msg(chat_id, mt.finish_cmd_first(), reply_markup=custom_kb)
                             else:
@@ -89,17 +88,17 @@ class Bot(BaseBot):
                                     set_user(self, chat_id, "state", 6)
 
                                 else:
-                                    # böyle bir komut yok
+                                    # Böyle bir komut yok
                                     self.send_msg(chat_id, mt.could_not_understand())
                         else:
-                            # kullanıcı veritabanında yok
+                            # Kullanıcı veritabanında yok
                             self.send_msg(chat_id, mt.not_met(), reply_markup=self.api.build_remove_keyboard())
                 else:
                     if user_exists(self, chat_id):
                         user_state = get_user(self, chat_id, "state")[0]
 
                         if user_state == 1:
-                            # kullanıcı ismini yazdı
+                            # Kullanıcı ismini yazdı
                             user_name = sanitize_and_validate_user_name(text)
                             if user_name != None:
                                 set_user(self, chat_id, "name", user_name)
@@ -107,11 +106,11 @@ class Bot(BaseBot):
                                 self.send_msg(chat_id, mt.ask_faculty(), reply_markup=custom_kb)
                                 set_user(self, chat_id, "state", 2)
                             else:
-                                # isim uygun değil
+                                # İsim uygun değil
                                 self.send_msg(chat_id, mt.invalid_user_name(), reply_markup=self.api.build_remove_keyboard())
 
                         elif user_state == 2:
-                            # kullanıcı fakülte seçti
+                            # Kullanıcı fakülte seçti
                             faculty_row = get_faculty_by_name(self, text, "id")
                             if faculty_row:
                                 faculty_id = faculty_row[0]
@@ -120,12 +119,12 @@ class Bot(BaseBot):
                                 self.send_msg(chat_id, mt.ask_department(), reply_markup=custom_kb)
                                 set_user(self, chat_id, "state", 3)
                             else:
-                                # özel klavyeden seçmedi (böyle bir fakülte yok)
+                                # Özel klavyeden seçmedi (böyle bir fakülte yok)
                                 custom_kb = build_custom_kb_of_faculties(self)
                                 self.send_msg(chat_id, mt.invalid_response_use_keyboard(), reply_markup=custom_kb)
 
                         elif user_state == 3:
-                            # kullanıcı bölüm seçti
+                            # Kullanıcı bölüm seçti
                             department_row = get_department_by_name(self, text, "id")
                             if department_row:
                                 department_id = department_row[0]
@@ -138,7 +137,7 @@ class Bot(BaseBot):
                                 self.send_msg(chat_id, mt.meeting_done(user_name), reply_markup=self.api.build_remove_keyboard())
                                 set_user(self, chat_id, "state", 4)
                             else:
-                                # özel klavyeden seçmedi (böyle bir bölüm yok)
+                                # Özel klavyeden seçmedi (böyle bir bölüm yok)
                                 faculty_id = get_user(self, chat_id, "faculty_id")[0]
                                 department_list = [row[0] for row in get_departments_by_faculty_id(self, faculty_id, "name")]
                                 department_list.sort()
@@ -146,32 +145,32 @@ class Bot(BaseBot):
                                 self.send_msg(chat_id, mt.invalid_response_use_keyboard(), reply_markup=custom_kb)
 
                         elif user_state == 5:
-                            # kullanıcı ayarlamalar yapıyor
+                            # Kullanıcı ayarlamalar yapıyor
                             if text.strip().lower() in [mt.ayarla_done_button().lower(), "tamamla"]:
-                                # tamamla
+                                # Tamamla
                                 self.api.delete_message(chat_id, get_user(self, chat_id, "bot_last_msg_id")[0])
                                 self.send_msg(chat_id, mt.ayarla_done(), reply_markup=self.api.build_remove_keyboard())
                                 set_user(self, chat_id, "state", 4)
                             else:
-                                # kanal aç/kapa
+                                # Kanal aboneliğini aç/kapat
                                 channel_row = get_channel_by_name(self, text[:-2], "id")
                                 if channel_row:
                                     channel_id = channel_row[0]
                                     toggle_subscription_alt(self, chat_id, channel_id)
-                                # aşağıdaki 3 satır iki durumda da ortak, o yüzden if'i ikiye böldüm
+                                # (Aşağıdaki 3 satır iki durumda da ortak, o yüzden if'i ikiye böldüm)
                                 custom_kb = build_custom_kb_for_ayarla(self, chat_id)
-                                self.api.delete_message(chat_id, msg_id)  # kullanıcının mesajını (mesela "Genel Duyurular") sil
-                                self.api.delete_message(chat_id, get_user(self, chat_id, "bot_last_msg_id")[0])  # botun mesajını (mesela "...") sil
+                                self.api.delete_message(chat_id, msg_id)  # Kullanıcının mesajını (mesela "Genel Duyurular") sil
+                                self.api.delete_message(chat_id, get_user(self, chat_id, "bot_last_msg_id")[0])  # Botun mesajını (mesela "...") sil
                                 if channel_row:
                                     ok, sent_msg = self.send_msg(chat_id, mt.ayarla_waiting(), reply_markup=custom_kb)
                                 else:
-                                    # özel klavyeden seçmedi (böyle bir kanal yok)
+                                    # Özel klavyeden seçmedi (böyle bir kanal yok)
                                     ok, sent_msg = self.send_msg(chat_id, mt.invalid_response_use_keyboard(), reply_markup=custom_kb)
                                 if ok:
                                     set_user(self, chat_id, "bot_last_msg_id", sent_msg["result"]["message_id"])
 
                         elif user_state == 6:
-                            # kullanıcıdan sıfırlama için onay alınıyor
+                            # Kullanıcıdan sıfırlama için onay alınıyor
                             if text.strip().lower() == "onayla":
                                 delete_user(self, chat_id)
                                 self.send_msg(chat_id, mt.sifirla_done(), reply_markup=self.api.build_remove_keyboard())
@@ -180,16 +179,16 @@ class Bot(BaseBot):
                                 set_user(self, chat_id, "state", 4)
 
                         else:
-                            # kullanıcıdan mesaj beklemiyorduk
+                            # Kullanıcıdan mesaj beklemiyorduk
                             self.send_msg(chat_id, mt.could_not_understand())
                     else:
-                        # kullanıcı veritabanında yok
+                        # Kullanıcı veritabanında yok
                         self.send_msg(chat_id, mt.not_met(), reply_markup=self.api.build_remove_keyboard())
 
             else:
-                # garip durum: gelen mesajda chat_id veya text yok
+                # Beklenmedik durum: Gelen mesajda text yok
                 pprint(update)
                 self.send_msg(chat_id, mt.could_not_understand_msg())
         else:
-            # garip durum: gelen olay mesaj değil
+            # Beklenmedik durum: Gelen olay mesaj değil
             pprint(update)
